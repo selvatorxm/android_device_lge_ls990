45d623c531e94673411291f308a6a64dfa145dac
Mehmet KÖKLÜ <selvatorxm@gmail.com>
2018-02-16 17:09:19 +0300

    AOKP Compatible

---
+++ aokp.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/lge/ls990/full_ls990.mk)
+
+# Inherit some common Aokp stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+PRODUCT_NAME := aokp_ls990
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_spr_us" \
+    BUILD_FINGERPRINT="lge/g3_spr_us/g3:4.4.2/KVT49L.LS990ZV4/LS990ZV4.1403169216:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_spr_us-user 4.4.2 KVT49L.LS990ZV4 LS990ZV4.1403169216 release-keys"
